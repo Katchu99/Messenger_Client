13e7ee4a96a54d4bb4ae189276558e98554f176f
Marius <Maariius@outlook.de>
2024-06-12 22:29:17 +0200

    added snackbar

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,90 +1,108 @@
-<template>
-    <div class="body">
-        <div class="authContainer">
-            <div class="header" v-if="authState == 0">
-                LOGIN
-            </div>
-            <div class="header" v-if="authState == 1">
-                REGISTER
-            </div>
-            <div>
-                <div v-if="authState == 0">
-                    <Login />
-                </div>
-                <div v-if="authState == 1">
-                    <Register/>
-                </div>
-            </div>
-            <div v-if="authState == 0" @click="incState">
-                Registrieren?
-            </div>
-            <div v-if="authState == 1" @click="decState">
-                Bereits einen Account?
-            </div>
-
-
-        </div>
-
-    </div>
-</template>
-
 <script lang="ts" setup>
-import Login from '../items/Login.vue';
-import Register from '../items/Register.vue';
-import { ref } from 'vue';
-import { useAuthState } from '../store/authStore';
+import Login from "../items/Login.vue";
+import Register from "../items/Register.vue";
+import Snackbar from "../items/Snackbar.vue";
+import { ref, onMounted, onBeforeMount, onBeforeUnmount } from "vue";
+import { useAuthState } from "../store/authStore";
 
-var authState  = ref(useAuthState().authState)
+var authState = ref(useAuthState().authState);
 
-const incState = ()=>{
-    authState.value += 1
+const incState = () => {
+  authState.value += 1;
+};
+
+const decState = () => {
+  authState.value -= 1;
+};
+
+const snackbarRef = ref<typeof Snackbar | null>(null);
+
+// const onShowSnackbar = ({message, color}: { message: string, color: string}) => {
+//     if (snackbarRef.value) {
+//         snackbarRef.value.showSnackbar(message, color);
+//     }
+// }
+
+function onShowSnackbar(message: String, color: String) {
+  if (snackbarRef.value) {
+    snackbarRef.value.showSnackbar(message, color);
+  }
 }
-
-const decState = ()=>{
-    authState.value -= 1
-}
-
 </script>
 
+<template>
+  <div class="body">
+    <div class="authContainer">
+      <div class="header" v-if="authState == 0">LOGIN</div>
+      <div class="header" v-if="authState == 1">REGISTER</div>
+      <div>
+        <div v-if="authState == 0">
+          <Login />
+        </div>
+        <div v-if="authState == 1">
+          <Register @showSnackbar="onShowSnackbar" />
+        </div>
+      </div>
+      <div v-if="authState == 0" @click="incState">Registrieren?</div>
+      <div v-if="authState == 1" @click="decState">Bereits einen Account?</div>
+    </div>
+    <Snackbar ref="snackbarRef" />
+  </div>
+</template>
 
-
-<style >
+<style>
 .body {
-    background-color: rgba(14, 13, 13, 0.733);
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0px;
-    left: 0px;
+  background-image: url("src/assets/background-login.jpg");
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+::-webkit-scrollbar {
+  display: none;
 }
 
 .authContainer {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: #f0f0f0;
-    border-radius: 5px;
-    box-shadow: 0 0 10px rgba(34, 34, 34, 0.719);
-    background-color: rgb(32, 37, 37);
-    width: 40%;
-    height: 50vh;
-    border-radius: 6px;
+  background-color: #f0f0f0;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(34, 34, 34, 0.719);
+  background-color: rgb(32, 37, 37);
+  padding: 20px;
+  display: inline-block;
+  text-align: center;
+  min-width: 300px;
+  position: absolute;
+  width: 350px;
 
-    .header {
-        border-radius: 6px 6px 0px 0px;
-        background-color: rgb(26, 25, 25);
-        top: 0px;
-        height: 4vh;
-        width: 100%;
-        display: flex;
-        align-content: center;
-        align-items: center;
-        justify-content: center;
-        font-weight: bold;
-        color: white;
-        font-size: 20px;
-        border-bottom: 1px solid rgba(128, 128, 128, 0.082);
-    }
+  .header {
+    border-radius: 6px 6px 0px 0px;
+    background-color: rgb(32, 37, 37);
+    padding: 10px;
+    font-weight: bold;
+    color: white;
+    font-size: 20px;
+    margin-bottom: 10px;
+  }
+
+  .content {
+    margin-bottom: 10px;
+  }
+
+  .input_field {
+    width: 100%;
+    box-sizing: border-box;
+  }
+
+  .toggle {
+    color: #00f;
+    cursor: pointer;
+    text-decoration: underline;
+  }
 }
 </style>
---
+++ src/items/Snackbar.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    data() {
+        return {
+            snackbar: false,
+            message: '',
+            timeout: 6000,
+            color: '',
+        };
+    },
+    methods: {
+        showSnackbar(message, color = 'success') {
+            this.message = message;
+            this.color = color;
+            this.snackbar = true;
+        },
+    },
+};
+</script>
+
+<template>
+    <v-snackbar v-model="snackbar" :time="timeout" :color="color">
+        <template #action="{ attrs}">
+            <v-btn color="#f0f0f0" text v-bind="attrs" @click="snackbar = false">
+                close
+            </v-btn>
+        </template>
+    </v-snackbar>
+</template>
+
+
+<style>
+</style>
