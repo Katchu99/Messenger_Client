eadb5fb28d5b25876dc6ba29e6afa1eceae27822
Marius <Maariius@outlook.de>
2024-06-12 22:28:04 +0200

    +googleapi fonts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
@@ -10,16 +9,19 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// Material-Icons
+import '@mdi/font/css/materialdesignicons.css'
+
 const vuetify = createVuetify({
-    components,
-    directives,
-  })
+  components,
+  directives,
+});
 
 //
 
 
 const app = createApp(App)
-app.use(createPinia()) //use pinia
-app.use(router) //use router
-app.use(vuetify)//vuetify
-app.mount('#app')
+app.use(createPinia()) //use Pinia
+app.use(router) //use Router
+app.use(vuetify) //use Vuetify
+app.mount('#app') //mount App
