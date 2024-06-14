ce10c489cfafb72421e2c9f5d187de0d5377741d
Marius <Maariius@outlook.de>
2024-06-14 16:27:33 +0200

    chat logic

---
+++ src/store/chatStore.ts
@@ -0,0 +1,33 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+import { Chat } from "../interfaces/Chat";
+
+export const useChatStore = defineStore("chat", {
+  state: () => ({
+    chats: [],
+    user: {
+      id: localStorage.getItem("user.id") || null,
+      name: localStorage.getItem("user.name") || null,
+    },
+    token: localStorage.getItem("token"),
+  }),
+  actions: {
+    async get_chats(): Promise<any> {
+      try {
+        axios.defaults.baseURL = "http://localhost:6969";
+        axios.defaults.headers.common["Authorization"] = `Bearer ${this.token}`;
+
+        const response = await axios.get(`/chat/${this.user.id}`);
+
+        const formattedChats = response.data.map((chat: Chat) => ({
+          chat_id: chat.chat_id,
+          chat_name: chat.chat_name,
+        }));
+
+        this.chats = formattedChats;
+      } catch (error) {
+        console.error("Error fetching chats:", error);
+      }
+    },
+  },
+});
