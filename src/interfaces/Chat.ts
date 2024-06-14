1537e49bfbd7653fbda7ebce75540d3809c53329
Marius <Maariius@outlook.de>
2024-06-14 16:27:40 +0200

    interface

---
+++ src/interfaces/Chat.ts
@@ -0,0 +1,28 @@
+// export interface history{
+//     icon:String
+//     content:chat | groupChat
+// }
+
+// interface chat{
+//     id:String,
+//     target:String,
+//     messages:Array<message>
+// }
+
+// interface groupChat{
+//     id:String,
+//     members:Array<String>,
+//     messages:Array<message>
+// }
+
+// interface message{
+//     author:String,
+//     message:String,
+//     timestamp:Date,
+//     position:Number
+// }
+
+export interface Chat {
+  chat_id: string;
+  chat_name: string;
+}
